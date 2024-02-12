7a4751b29b4dd43391ee6f5de5f306f7507410dc
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-12 20:51:39 +0530

    problem 4 bruteForce solution  , 2D Arr optimized TimeComplexity Added

---
+++ Arrays/BinarySearch/problem_4.js
@@ -0,0 +1,35 @@
+/*
+You are a product manager and currently leading a team to develop a new product.
+Unfortunately, the latest version of your product fails the quality check. Since each
+version is developed based on the previous version, all the versions after a bad version
+are also bad. Suppose you have n version and you want to find out the first bad one,
+which causes all the following ones to be bad. Also, talk about the time complexity of
+your code.
+
+Test Cases:
+Input: [0,0,0,1,1,1,1,1,1]
+Output: 3
+Explanation: 0 indicates a good version and 1 indicates a bad version. So, the index of
+the first 1 is at 3. Thus, the output is 3
+
+ */
+
+
+// Brute Force Solution Time Complexity - O(n)
+
+function findFirstFailedTest(arr,searchValue){
+
+    for(let i=0;i<arr.length;i++){
+
+        if(arr[i]==1){
+            return i
+        }
+    }
+    return -1
+}
+
+const testsArray = [0,0,0,1,1,1,1,1,1]
+
+const result = findFirstFailedTest(testsArray,searchValue=1)
+
+console.log(result)
