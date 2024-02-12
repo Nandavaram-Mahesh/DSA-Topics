c6202f12ba0476e550dd0cfeb0557674157b45fb
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-02-12 20:40:31 +0530

    Problem 2 Brute Force Solution added and Two Dim Array Optimized Sol added

--- Arrays/BinarySearch/problem_2.js
+++ Arrays/BinarySearch/problem_2.js
@@ -1,6 +1,33 @@
-// Two Pointer Approach
 
 
+/*
+1. Given a sorted Array
+2.Given a sum_value
+3. Find the total_pairs whose sum is equal to sum_value
+
+
+*/
+
+// BruteForce Approach TimeComplexity-O(n**2)
+
+function bruteForceFindingParis(arr,sum_value){
+    let pairs=0
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==sum_value){
+                pairs+=1
+            }
+        }
+    }
+    return pairs
+
+}
+
+// Moderate Solution
+
+
+
+// Optimized Way // Two Pointer Approach
 function findpairs(arr,left,right,sum_value){
 
     while(left<=right){
@@ -21,8 +48,16 @@ function findpairs(arr,left,right,sum_value){
 
 
 
-const sortedArray = [20,40,60,80,90,120,240]
+const sortedArray = [20,40,60,70,80,90,120,140,240]
 
 
-result = findpairs(sortedArray,left=0,right=(sortedArray.length)-1,sum_value=210)
-console.log(result)
+const result = findpairs(sortedArray,left=0,right=(sortedArray.length)-1,sum_value=210)
+// console.log(result)
+
+const bruteForceResult = bruteForceFindingParis(sortedArray,sum_value=140)
+
+console.log(`bruteForceResult:${bruteForceResult}`)
+
+const moderateResult = moderateFindingPairs(sortedArray,left=0,right=(sortedArray.length)-1,sum_value=210)
+
+console.log(`moderateResult:${moderateResult}`)
