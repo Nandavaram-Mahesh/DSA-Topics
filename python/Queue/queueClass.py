1b03f632fa95dfc99884e9c5d48fcd9add3310a4
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-03-26 15:57:22 +0530

    Queue Class Implementation

---
+++ python/Queue/queueClass.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self,val):
+        return self.queue.append(val)
+
+    def dequeue(self):
+        return self.queue.popleft()
+
+    def peek(self):
+        return self.queue[0]
+    def is_Empty(self):
+        return len(self.queue)==0
+
+    def size(self):
+        return len(self.queue)
+    
+
+
+q = Queue()
+
+q.enqueue(142)
+q.enqueue(150)
+q.enqueue(162)
+q.enqueue(182)
+q.enqueue(190)
+
+q.dequeue()
+q.dequeue()
+queueValue = q.peek()
+print(queueValue) 
