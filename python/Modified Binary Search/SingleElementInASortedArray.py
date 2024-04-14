31965377c9fab817313c1bf1472b2bc1dcb27d09
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-04-14 10:42:58 +0530

    Single Element  - HashMapApproach

--- python/Modified Binary Search/SingleElementInASortedArray.py
+++ python/Modified Binary Search/SingleElementInASortedArray.py
@@ -7,28 +7,46 @@ def findSingleElement(arr,left,right):
     
     # Two pointers Approach - Time Complexity - O(N)
 
-    while right<len(arr):
+    # while right<len(arr):
 
-        if len(arr)==1:
-            return 0
+    #     if len(arr)==1:
+    #         return 0
         
-        if(len(arr)<3):
-            if(arr[left]==arr[right]):
-                return None
+    #     if(len(arr)<3):
+    #         if(arr[left]==arr[right]):
+    #             return None
+    #         else:
+    #             return[0,1]
+
+    #     if(arr[left]==arr[right]):
+    #         if(left==len(arr)-3):
+    #              return right+1
+    #         left = right+1
+    #         right = left+1
+    #     else:
+    #         return left
+
+    # ----------------------------------------------------------------------
+
+    # HashMap Approach - Time Complexity - O(N) , Space Complexity - O(N) 
+        
+        hashMap = {}
+        for i in arr:
+            if i in hashMap:
+                  hashMap[i] += 1
             else:
-                return[0,1]
+                  hashMap[i] = 1 
+        
+        # return hashMap
+        singleElem = 0
+        for key in hashMap.keys():
+              if(hashMap[key]==1):
+                singleElem = key
+        
+        return arr[singleElem]
 
-        if(arr[left]==arr[right]):
-            if(left==len(arr)-3):
-                 return right+1
-            left = right+1
-            right = left+1
-        else:
-            return left
+ 
 
-
-    
-    pass
 
 
 
