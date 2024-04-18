271085db849c64576e0983d3052959fbaa970c84
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2024-04-18 06:22:45 +0530

    InOrder Travesal Implementation

--- python/Trees/Logic.py
+++ python/Trees/Logic.py
@@ -13,10 +13,6 @@ def insertNodeIntoSubTree(node,new_node_data,subTreeflag):
             node.right = Node(new_node_data)
             return node.right
 
-
-        
-
-
 def preOrderTraversal(root):
 
     if root:
@@ -24,6 +20,13 @@ def preOrderTraversal(root):
         preOrderTraversal(root.left)
         preOrderTraversal(root.right) 
     
+def inOrderTraversal(root):
+    
+    if root:
+        inOrderTraversal(root.left)
+        print(root.nodeData)
+        inOrderTraversal(root.right)
+
 
 
 node = Node(10)
@@ -37,5 +40,8 @@ root_node_left_right = insertNodeIntoSubTree(root_node_left,50,subTreeflag=False
 root_node_right_left = insertNodeIntoSubTree(root_node_right,60,subTreeflag=True)
 root_node_right_right = insertNodeIntoSubTree(root_node_right,70,subTreeflag=False)
 
+# Working Fine - Output: 10 20 40 50 30 60 70
+# preOrderTraversal(node)
 
-preOrderTraversal(node)
+# Output: 40 20 50 10 60 30 70
+inOrderTraversal(node)
